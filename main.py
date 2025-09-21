8a7a28baef769e4b81a7dfc1e16885672608e901
Andreas <seidelandreas001@gmail.com>
2025-09-21 09:26:43 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "http://localhost:11434/api/generate"
+resp = requests.post(
+    url,
+    json={
+        "model": "dolphin-llama3",  # 👈 model slug here
+        "prompt": "Write a haiku about local llms.",
+        "stream": False,  # set True for streaming responses
+    },
+)
+
+print(resp.json()["response"])
