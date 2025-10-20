b0ce0d5e9c3c12b332f2243c49dfe1f9ebabbf45
Andreas <seidelandreas001@gmail.com>
2025-10-20 20:03:22 +0200

    begin implementing llm service and ollama config

--- app/llm_service.py
+++ app/llm_service.py
@@ -3,6 +3,45 @@ import subprocess
 import time
 from pathlib import Path
 import stat
+import os
+import sys
+
+
+def start_ollama_server():
+    user_os = sys.platform()
+
+    if user_os in ["linux", "darwin"]:
+        script = Path("/home/andreas/Documents/local_llm/serve_ollama_posix.sh")
+        try:
+            script.chmod(script.stat().st_mode | stat.S_IEXEC)
+
+            subprocess.Popen(
+                ["bash", "/home/andreas/Documents/local_llm/serve_ollama_posix.sh"],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+            )
+            print("ollama starting")
+            time.sleep(3)
+        except:
+            print("error occurred")
+
+    elif user_os in ["win32", "cygwin"]:
+        script = Path("/home/andreas/Documents/local_llm/serve_ollama_windows.ps1")
+        process = subprocess.Popen(
+            ["powershell.exe", "-ExecutionPolicy", "Bypass", "-File", script],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        )
+
+        # Read the output
+        stdout, stderr = process.communicate()
+
+        print("ollama starting")
+        time.sleep(3)
+
+    else:
+        print(f"Operating System {os} not supported at the moment")
 
 
 def fetch_ollama():
---
+++ app/serve_ollama_windows.ps1
@@ -0,0 +1,5 @@
+# Set environment variable
+$env:OLLAMA_HOME = "C:\Users\Andreas\Documents\Python\local_llm\models\ollama_runtime\ollama"
+
+# Run the Ollama server
+ollama serve
