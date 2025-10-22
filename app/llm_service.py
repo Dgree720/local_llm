2d037cedbdf10df1e66a8df362783ec147381216
Andreas <seidelandreas001@gmail.com>
2025-10-22 20:29:52 +0200

    llm service fixes with ollama server startup

--- app/llm_service.py
+++ app/llm_service.py
@@ -15,84 +15,88 @@ import requests as re
 
 class LLM_Service:
     def __init__(self):
+        os.environ["OLLAMA_MODELS"] = (
+            "/home/andreas/Documents/Python/local_llm/models/llm/ollama"
+        )
+        os.environ["OLLAMA_HOST"] = "http://127.0.0.1:11434"
+
         self.server = self.start_ollama_server()
         self.llm = self.fetch_llm()
         self.prompt = make_prompt()
 
     def start_ollama_server(self):
         # check if instance of ollama server is already running
-        response = re.get("http://localhost:11434/")
-
-        if response.status_code == 200:
+        try:
+            response = re.get("http://127.0.0.1:11434")
+            print("Ollama server already running")
             return True
+        except Exception:
+            print("Starting Ollama server")
+        user_os = sys.platform
+
+        if user_os in ["linux", "darwin"]:
+            print(f"User OS recognized as {user_os}")
+            script = Path(
+                "/home/andreas/Documents/Python/local_llm/app/serve_ollama_posix.sh"
+            )
+            try:
+                script.chmod(script.stat().st_mode | stat.S_IEXEC)
+
+                subprocess.Popen(
+                    [
+                        "bash",
+                        "/home/andreas/Documents/Python/local_llm/app/serve_ollama_posix.sh",
+                    ],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL,
+                )
+                print("ollama starting")
+                time.sleep(10)
+                return True
+            except Exception as e:
+                print(f"error occurred: {e}")
+                return False
+
+        elif user_os in ["win32", "cygwin"]:
+            script = Path("/home/andreas/Documents/local_llm/serve_ollama_windows.ps1")
+
+            try:
+                process = subprocess.Popen(
+                    [
+                        "powershell.exe",
+                        "-ExecutionPolicy",
+                        "Bypass",
+                        "-File",
+                        script,
+                    ],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    text=True,
+                )
+
+                print("ollama starting")
+                time.sleep(3)
+                return True
+
+            except Exception as e:
+                print(f"error occurred: {e}")
+                return False
 
         else:
-            user_os = sys.platform
-
-            if user_os in ["linux", "darwin"]:
-                print(f"User OS recognized as {user_os}")
-                script = Path(
-                    "/home/andreas/Documents/Python/local_llm/app/serve_ollama_posix.sh"
-                )
-                try:
-                    script.chmod(script.stat().st_mode | stat.S_IEXEC)
-
-                    subprocess.Popen(
-                        [
-                            "bash",
-                            "/home/andreas/Documents/Python/local_llm/app/serve_ollama_posix.sh",
-                        ],
-                        stdout=subprocess.DEVNULL,
-                        stderr=subprocess.DEVNULL,
-                    )
-                    print("ollama starting")
-                    time.sleep(3)
-                    return True
-                except Exception as e:
-                    print(f"error occurred: {e}")
-                    return False
-
-            elif user_os in ["win32", "cygwin"]:
-                script = Path(
-                    "/home/andreas/Documents/local_llm/serve_ollama_windows.ps1"
-                )
-
-                try:
-                    process = subprocess.Popen(
-                        [
-                            "powershell.exe",
-                            "-ExecutionPolicy",
-                            "Bypass",
-                            "-File",
-                            script,
-                        ],
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE,
-                        text=True,
-                    )
-
-                    print("ollama starting")
-                    time.sleep(3)
-                    return True
-
-                except Exception as e:
-                    print(f"error occurred: {e}")
-                    return False
-
-            else:
-                print(f"Operating System {os} not supported at the moment")
-                return False
+            print(f"Operating System {os} not supported at the moment")
+            return False
 
     def fetch_llm(self):
         llm = OllamaLLM(model="gemma3:1b", temperature=0.4)
+
+        print(f"LLM {llm.model} running")
 
         return llm
 
     def get_response(self, user_query, context):
         prompt = PromptTemplate(
-            template=self.prompt, input_variables=["user_query, context"]
+            template=self.prompt, input_variables=["user_query", "context"]
         )
-
         chain = (
             {
                 "user_query": RunnablePassthrough(),
--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
-import requests
-from utils.download_models import download_models
-from utils.configuration import config
-from prompts import make_prompt
 from llm_service import LLM_Service
 from rag_service import Retriever
 import sys
 from omegaconf import OmegaConf
+import os
+import time
+
+os.environ["OLLAMA_MODELS"] = (
+    "/home/andreas/Documents/Python/local_llm/models/llm/ollama"
+)
 
 
 def main():
@@ -20,11 +22,17 @@ def main():
     llm = LLM_Service()
     ret = Retriever(config.retriever_config)
     user_query = input("input: ")
-    retrieved_context = ret.retrieve(user_query)
+    retrieved_context = "You are a large language model"  # ret.retrieve(user_query)
+    # print(retrieved_context)
 
+    start_time = time.time()
     response = llm.get_response(user_query, retrieved_context)
 
     print(response)
+
+    end_time = time.time()
+    time_elapsed = end_time - start_time
+    print(time_elapsed)
 
 
 if __name__ == "__main__":
