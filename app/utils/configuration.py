62a3ac4bfb80347af7110c5a0eb266a2527aa4ab
Andreas <seidelandreas001@gmail.com>
2025-10-22 15:35:40 +0200

    begin updating configuration.py

--- app/utils/configuration.py
+++ app/utils/configuration.py
@@ -15,9 +15,15 @@ def config():
         with open(config_path, "r") as f:
             config = yaml.safe_load(f)
 
-        # define paths
+        # define configurations to change   
+        user_os = sys.platform
         models_path = f"{parent}/models"
         vector_store = os.path.join(os.getcwd(), "chroma_db")
+        knowledge_path: /home/andreas/Documents/Python/local_llm/documents
+        embed_model: "ibm-granite/granite-embedding-107m-multilingual"
+        embedding_model_path: /home/andreas/Documents/Python/local_llm/models/ibm-granite/granite-embedding-107m-multilingual
+        vector_db_path: /home/andreas/Documents/Python/local_llm/app/chroma_db
+        
 
         # write paths to config
         config["model_path"] = models_path
